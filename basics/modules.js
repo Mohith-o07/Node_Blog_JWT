02b1b2aa94049f14d5474fd0a70ed2e0f41ff6e0
Mohith Eswar Pyda <105948213+Mohith-o07@users.noreply.github.com>
2024-03-17 20:38:46 +0530

    Delete basics directory

--- basics/modules.js
+++
@@ -1,7 +0,0 @@
-const {people,ages}=require('../people');
-//console.log(people); //we can't access other file's vars..
-console.log(people);
-console.log(ages);
-const os=require('os')
-//console.log(os);
-console.log(os.homedir(),os.platform());
--- basics/people.js
+++
@@ -1,6 +0,0 @@
-const people=['radha','krishna','nanda','rohini','yashoda'];
-const ages=[1,2,3,4,5];
-//console.log(people);
-module.exports={
-    people,ages
-};
